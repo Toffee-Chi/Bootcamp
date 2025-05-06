d75b2d43b8a5c245d708f517f49fa55f10901508
ToffeeChi <kevinauterig@gmail.com>
2025-05-06 14:55:49 -0300

    LazyColumn
    Image, Column, Items, pequeños usos del modifier

--- Kotlin/Compose/ColumnAplication/app/src/main/java/com/example/columnapplication/MainActivity.kt
+++ Kotlin/Compose/ColumnAplication/app/src/main/java/com/example/columnapplication/MainActivity.kt
@@ -1,47 +1,64 @@
 package com.example.columnapplication
 
 import android.os.Bundle
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.columnapplication.ui.theme.ColumnApplicationTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ColumnApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            app()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ColumnApplicationTheme {
-        Greeting("Android")
+fun app() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFA500))
+            .padding(16.dp)
+    ) {
+        item {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_orange_kawai),
+                    contentDescription = "orange",
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+            }
+            Text(
+                text = "Columna 1",
+                fontSize = 32.sp,
+                color = Color.White,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            Text(text = "Columna 2")
+            Text(text = "Columna 3")
+        }
     }
 }
