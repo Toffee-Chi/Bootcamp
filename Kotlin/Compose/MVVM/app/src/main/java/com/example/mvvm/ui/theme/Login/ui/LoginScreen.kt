e0b7659d40079b77f4fab2a79e6c2c932fe9dc6b
ToffeeChi <kevinauterig@gmail.com>
2025-05-16 16:53:16 -0300

    MVVM basico
    Probando un simple login con la estructura mvvm

---
+++ Kotlin/Compose/MVVM/app/src/main/java/com/example/mvvm/ui/theme/Login/ui/LoginScreen.kt
@@ -0,0 +1,130 @@
+package com.example.mvvm.ui.theme.Login.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mvvm.R
+import kotlinx.coroutines.launch
+
+@Composable
+fun LoginScreen(viewModel: LoginViewModel) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Login(Modifier.align(Alignment.Center), viewModel)
+    }
+}
+
+@Composable
+fun Login(modifier: Modifier, viewModel: LoginViewModel) {
+    val email: String by viewModel.email.observeAsState(initial = "")
+    val password: String by viewModel.password.observeAsState(initial = "")
+    val loginEnable: Boolean by viewModel.loginEnable.observeAsState(initial = false)
+
+    val isLoading: Boolean by viewModel.isLoading.observeAsState(initial = false)
+
+    val coroutineScope = rememberCoroutineScope()
+
+    if (isLoading) {
+        Box(Modifier.fillMaxSize()) {
+            CircularProgressIndicator(Modifier.align(Alignment.Center))
+        }
+    } else {
+        Column(modifier = modifier) {
+            HeaderImage(Modifier.align(Alignment.CenterHorizontally))
+            Spacer(modifier = Modifier.padding(16.dp))
+            EmailField(email) { viewModel.onLoginChanged(it, password) }
+            Spacer(modifier = Modifier.padding(16.dp))
+            PassField(password) { viewModel.onLoginChanged(email, it) }
+            Spacer(modifier = Modifier.padding(8.dp))
+            ForgotPassword(Modifier.align(Alignment.End))
+            Spacer(modifier = Modifier.padding(16.dp))
+            LoginButton(loginEnable) {
+                coroutineScope.launch {
+                    viewModel.onLoginSelected()
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginButton(loginEnable: Boolean, onLoginSelected: () -> Unit) {
+    Button(
+        onClick = { onLoginSelected },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFFF4303),
+            disabledContainerColor = Color(0xFFF78058)
+        ), enabled = loginEnable
+    ) {}
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "Olvidaste la contraseña",
+        modifier = modifier.clickable {},
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFFFFC107)
+    )
+}
+
+@Composable
+fun PassField(password: String, onTextFieldChanged: (String) -> Unit) {
+    TextField(
+        value = password, onValueChange = { onTextFieldChanged(it) },
+        placeholder = { Text(text = "Contraseña") },
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = Color.White,
+            unfocusedTextColor = Color.White
+        )
+    )
+}
+
+@Composable
+fun EmailField(email: String, onTextFieldChanged: (String) -> Unit) {
+    TextField(
+        value = email, onValueChange = { onTextFieldChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = Color.White,
+            unfocusedTextColor = Color.White
+        )
+    )
+}
+
+@Composable
+fun HeaderImage(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_login),
+        contentDescription = "Icon Store",
+        modifier = modifier
+    )
+}
